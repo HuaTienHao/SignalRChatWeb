c831b923082ed61a432008b18a76f9ef294c4e8a
Admin <huatienhao095@gmail.com>
2023-10-09 19:25:55 +0700

    Add project files.

---
+++ SignalRChatWeb/ChatHub.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRChatWeb
+{
+    public class ChatHub : Hub
+    {
+        private static Dictionary<string, string> connectedClients = new Dictionary<string, string>();
+        // Send notification to all clients
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        // Everyone will be notified except who have joined the chat
+        public async Task JoinChat(string user, string message)
+        {
+            connectedClients[Context.ConnectionId] = user;
+            await Clients.Others.SendAsync("ReceiveMessage", user, message);
+        }
+
+        private async Task LeaveChat()
+        {
+            if (connectedClients.TryGetValue(Context.ConnectionId, out string user))
+            {
+                var message = $"{user} left the chat";
+                await Clients.Others.SendAsync("ReceiveMessage", user, message);
+            }
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            await LeaveChat();
+            await base.OnDisconnectedAsync(exception);
+        }
+    }
+}
